c03b7d382218c646f6c7bb87e710f7dcbb2c86b1
Christian Scholtysik <christianscholtysik@gmail.com>
2024-08-04 14:37:25 +0200

    add content and css to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,14 @@
 import BottomNavBar from "../../components/BottomNav";
+import CategoryButtons from "../../components/CategoryButtons";
+import SearchInput from "../../components/SearchInput";
+import "./HomePage.css";
 
 const Home = () => {
   return (
-    <section>
+    <section className="flex flex-col h-full">
+      <h1 className=" header-home text-3xl text-gray-950">Welcome!</h1>
+      <SearchInput />
+      <CategoryButtons />
       <BottomNavBar />
     </section>
   );

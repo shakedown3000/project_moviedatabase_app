16d5d18a9c45fb898a692af66ff13c7adfdd3936
shakedown3000 <annalaube90@gmail.com>
2024-08-07 00:33:10 +0200

    edit HomePage Gallery

--- src/components/MovieImage.tsx
+++ src/components/MovieImage.tsx
@@ -1,38 +1,21 @@
-import "./MovieImage.css";
+import React from "react";
+import { IResult } from "../../Interfaces/ITrendingMovies";
 
 interface MovieImageProps {
-  src: string;
-  alt: string;
-  title: string;
-  release_date: string;
-  vote_average: number;
+  movie: IResult;
 }
 
-const MovieImage: React.FC<MovieImageProps> = ({
-  src,
-  alt,
-  title,
-  release_date,
-  vote_average,
-}) => {
-  const backgroundImage = `https://image.tmdb.org/t/p/original${src}`;
+const SingleTrendingMovie: React.FC<MovieImageProps> = ({ movie }) => {
   return (
-    <section className="">
-      <div
-        className="movieImage hero rounded-3xl bg-cover bg-top h-full"
-        style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <section className="hero-overlay bg-opacity-30 ">
-          <div className="text-white text-m mb-2 text-center mr-3 mt-10 ml-3  ">
-            {title}
-            {/* {new Date(release_date).getFullYear()} */}
-          </div>
-          <div className=" text-yellow-400 text-s text-right mr-3 ">
-            ⭐️ {vote_average.toFixed(1)} / 10.0
-          </div>
-        </section>
-      </div>
-    </section>
+    <div className="movie-image text-center">
+      <img
+        className="w-full h-auto rounded-3xl"
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+      />
+      <h3 className="mt-2 text-lg line-clamp-2">{movie.title}</h3>
+    </div>
   );
 };
 
-export default MovieImage;
+export default SingleTrendingMovie;

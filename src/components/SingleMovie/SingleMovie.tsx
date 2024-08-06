5954569d8fe0cbec00fc2f4fa3d6c6ce244db174
Christian Scholtysik <christianscholtysik@gmail.com>
2024-08-06 21:45:33 +0200

    add dynamic content  and routing to singleMovie

--- src/components/SingleMovie/SingleMovie.tsx
+++ src/components/SingleMovie/SingleMovie.tsx
@@ -44,75 +44,80 @@
 
 // export default SingleMovies;
 
+import { Link, NavLink } from "react-router-dom";
+import { IMovieDetail } from "../../Interfaces/IMovieDetails";
 
-import { Result } from "../../Interfaces/ISearchMovie";
 import "./SingleMovie.css/";
 interface SingleMoviesProps {
-  movie: Result;
+  movie: IMovieDetail;
 }
 
 const SingleMovies: React.FC<SingleMoviesProps> = ({ movie }) => {
+  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  console.log(movie.genres);
   return (
-    <section className="p-6 gap-5">
-      <div className="grid grid-cols-[repeat(11,2fr)] grid-rows-[repeat(3,1fr)] gap-1 h-36 p-4">
-        <div
-            className="col-start-1 col-end-4 col-span-3 row-start-1 row-span-3 h-full w-full bg-contain bg-center bg-no-repeat"
+    <NavLink to={`/detail/${movie.id}`}>
+      <section className="p-6 gap-5 ">
+        <div className="grid grid-cols-[repeat(11,2fr)] grid-rows-[repeat(3,1fr)] gap-1 h-36 p-4">
+          <div
+            className="col-start-1 col-end-4 col-span-3 row-start-1 row-span-3 h-full w-full bg-contain bg-center bg-no-repeat "
             style={{
-              backgroundImage: 'url("/Movie_Picture.png")',
-            }}
-        ></div>
+              backgroundImage: `url(${imageUrl})`,
+            }}></div>
 
-        <div className= "col-start-4 col-end-11 row-start-1 row-end-2">
-          <h3 className=" text-xl mb-4 font-bold">{movie.title}</h3>
-        </div>
-        <div className="col-start-11 col-end-12 row-start-1 row-end-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="-4 6 24 24"
-            strokeWidth={1.5}
-            className="size-8 icon-stroke icon-fill icon-stroke-hover icon-fill-hover">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-            />
-          </svg>
-        </div>
-        
-        <div className="col-start-4 col-end-5 row-start-2 row-end-2">
-          <img className="" src="/public/Star.svg" alt="Star" />
-        </div>
-        <div className="col-start-5 col-end-6 row-start-2 row-end-2 ">
-          <p className="font-bold">{movie.vote_average.toFixed(1)}</p>
-        </div>
-        <div className="col-start-6 col-end-7 row-start-2 row-end-2 ">
-          <p>•</p>
-        </div>
-        <div className="col-start-7 col-end-8 row-start-2 row-end-2 ">
-          <p className="col-start-3 col-span-1">{movie.release_date.toString().split("-")[0]}</p>
-        </div>
-        <div className="col-start-8 col-end-9 row-start-2 row-end-2 ">
-          <p>•</p>
-        </div>
+          <div className="col-start-4 col-end-11 row-start-1 row-end-2">
+            <h3 className=" text-xl mb-4 font-bold text-left ml-2">
+              {movie.title}
+            </h3>
+          </div>
+          <div className="col-start-11 col-end-12 row-start-1 row-end-2 ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="-4 6 24 24"
+              strokeWidth={1.5}
+              className="size-8 icon-stroke icon-fill icon-stroke-hover icon-fill-hover">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+              />
+            </svg>
+          </div>
+
+          <div className="col-start-4 col-end-5 row-start-2 row-end-2 ">
+            <img className="" src="/public/Star.svg" alt="Star" />
+          </div>
+          <div className="col-start-5 col-end-6 row-start-2 row-end-2 ">
+            <p className="font-bold">{movie.vote_average.toFixed(1)}</p>
+          </div>
+          <div className="col-start-6 col-end-7 row-start-2 row-end-2 ">
+            <p>•</p>
+          </div>
+          <div className="col-start-7 col-end-8 row-start-2 row-end-2 ">
+            <p className="col-start-3 col-span-1">
+              {movie.release_date.toString().split("-")[0]}
+            </p>
+          </div>
+          <div className="col-start-8 col-end-9 row-start-2 row-end-2 ">
+            <p>•</p>
+          </div>
           {/* Brauchen Enum */}
-        <div className="col-start-9 col-end-10 row-start-2 row-end-2 ">
-          <p className="col-start-4 col-span-1">{movie.genre_ids.join(", ")}</p>
-        </div>
-        <div className="col-start-10 col-end-11 row-start-2 row-end-2 ">
-          <p>•</p>
-        </div>
+          <div className="col-start-9 col-end-10 row-start-2 row-end-2 ">
+            {/* <p className="col-start-4 col-span-1">{movie.genres.join(", ")}</p> */}
+            {/* <p className="col-start-4 col-span-1">{movie?.genres.}</p> */}
+          </div>
+          <div className="col-start-10 col-end-11 row-start-2 row-end-2 ">
+            <p>•</p>
+          </div>
           {/* Hier bitte auf die Zeitumwandlung achten! */}
-        <div className="col-start-11 col-end-12 row-start-2 row-end-2 ">
-          <p className="col-start-5 col-span-1">Dauer</p>
+          <div className="col-start-11 col-end-12 row-start-2 row-end-2 ">
+            <p className="col-start-5 col-span-1">{movie.runtime}</p>
+          </div>
         </div>
-      </div>
-
-
-
-    </section>
+      </section>
+    </NavLink>
   );
 };
 
 export default SingleMovies;
-
--- src/pages/GenrePage/GenrePage.tsx
+++ src/pages/GenrePage/GenrePage.tsx
@@ -1,3 +1,42 @@
+// import React from "react";
+// import { useLocation } from "react-router-dom";
+// import BottomNavBar from "../../components/BottomNav";
+// import CategoryButtons from "../../components/CategoryButtons";
+// import SearchInput from "../../components/SearchInput";
+// import SingleMovies from "../../components/SingleMovie/SingleMovie";
+// import { ISearchMovie } from "../../Interfaces/ISearchMovie";
+
+// interface LocationState {
+//   results?: ISearchMovie;
+// }
+
+// const GenrePage: React.FC = () => {
+//   const location = useLocation();
+
+//   // Typ für den Zustand, den wir aus der Navigation erwarten
+//   const { results } = location.state as LocationState;
+//   console.log("Results STate", results);
+
+//   return (
+//     <div className="flex flex-col h-full relative">
+//       <SearchInput />
+//       <CategoryButtons />
+//       <div className="flex-1 mt-5 overflow-y-auto">
+//         {results?.results.length > 0 ? (
+//           results.results.map((movie) => (
+//             <SingleMovies key={movie.id} movie={movie} />
+//           ))
+//         ) : (
+//           <div>No results found</div>
+//         )}
+//       </div>
+//       <BottomNavBar />
+//     </div>
+//   );
+// };
+
+// export default GenrePage;
+
 import React from "react";
 import { useLocation } from "react-router-dom";
 import BottomNavBar from "../../components/BottomNav";
@@ -11,18 +50,20 @@ interface LocationState {
 }
 
 const GenrePage: React.FC = () => {
-  const location = useLocation();
+  const location = useLocation<LocationState>();
 
-  // Typ für den Zustand, den wir aus der Navigation erwarten
-  const { results } = location.state as LocationState;
-  console.log("Results STate", results);
+  const results = location.state?.results;
+
+  if (!results) {
+    return <div>No results found</div>;
+  }
 
   return (
     <div className="flex flex-col h-full relative">
       <SearchInput />
       <CategoryButtons />
       <div className="flex-1 mt-5 overflow-y-auto">
-        {results?.results?.length > 0 ? (
+        {results.results.length > 0 ? (
           results.results.map((movie) => (
             <SingleMovies key={movie.id} movie={movie} />
           ))

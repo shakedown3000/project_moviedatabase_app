9f0dc69aa33234753f85d68d856a6a6109be59f8
shakedown3000 <annalaube90@gmail.com>
2024-08-07 12:00:09 +0200

    edit genre page single movies

--- src/pages/GenrePage/GenrePage.tsx
+++ src/pages/GenrePage/GenrePage.tsx
@@ -1,49 +1,9 @@
-// import React from "react";
-// import { useLocation } from "react-router-dom";
-// import BottomNavBar from "../../components/BottomNav";
-// import CategoryButtons from "../../components/CategoryButtons";
-// import SearchInput from "../../components/SearchInput";
-// import SingleMovies from "../../components/SingleMovie/SingleMovie";
-// import { ISearchMovie } from "../../Interfaces/ISearchMovie";
-
-// interface LocationState {
-//   results?: ISearchMovie;
-// }
-
-// const GenrePage: React.FC = () => {
-//   const location = useLocation();
-
-//   // Typ für den Zustand, den wir aus der Navigation erwarten
-//   const { results } = location.state as LocationState;
-//   console.log("Results STate", results);
-
-//   return (
-//     <div className="flex flex-col h-full relative">
-//       <SearchInput />
-//       <CategoryButtons />
-//       <div className="flex-1 mt-5 overflow-y-auto">
-//         {results?.results.length > 0 ? (
-//           results.results.map((movie) => (
-//             <SingleMovies key={movie.id} movie={movie} />
-//           ))
-//         ) : (
-//           <div>No results found</div>
-//         )}
-//       </div>
-//       <BottomNavBar />
-//     </div>
-//   );
-// };
-
-// export default GenrePage;
-
 import React from "react";
 import { useLocation } from "react-router-dom";
 import BottomNavBar from "../../components/BottomNav";
 import CategoryButtons from "../../components/CategoryButtons";
 import SearchInput from "../../components/SearchInput";
 import SingleMovies from "../../components/SingleMovie/SingleMovie";
-import { ISearchMovie } from "../../Interfaces/ISearchMovie";
 import { IMovieDetail } from "../../Interfaces/IMovieDetails";
 
 interface LocationState {
@@ -60,16 +20,18 @@ const GenrePage: React.FC = () => {
   }
 
   return (
-    <div className="flex flex-col h-full relative">
+    <div className="flex flex-col h-full relative bg-white p-6">
       <SearchInput />
       <CategoryButtons />
-      <div className="flex-1 mt-5 overflow-y-auto">
+      <div className="flex-1 mt-10 overflow-y-auto">
         {results.results.length > 0 ? (
           results.results.map((movie) => (
             <SingleMovies key={movie.id} movie={movie} />
           ))
         ) : (
-          <div>No results found</div>
+          <div className="Center">
+            <p>No results found</p>
+          </div>
         )}
       </div>
       <BottomNavBar />

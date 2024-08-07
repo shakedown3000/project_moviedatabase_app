02a7543ab772bf7ac350093a0d7aa4c694749fc2
Lisa.Ly <lisa.y.ly1688@gmail.com>
2024-08-07 09:42:07 +0200

    svg

--- src/components/SingleMovie/SingleMovie.tsx
+++ src/components/SingleMovie/SingleMovie.tsx
@@ -65,19 +65,20 @@ const SingleMovies: React.FC<SingleMoviesProps> = ({ movie }) => {
         <div className= "col-start-4 col-end-11 row-start-1 row-end-2">
           <h3 className=" text-xl mb-4 font-bold">{movie.title}</h3>
         </div>
-        <div className="col-start-11 col-end-12 row-start-1 row-end-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="-4 6 24 24"
-            strokeWidth={1.5}
-            className="size-8 icon-stroke icon-fill icon-stroke-hover icon-fill-hover">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-            />
-          </svg>
+        <div className="col-start-11 col-end-12 row-start-1 row-end-2 content-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          className="size-8 icon-stroke icon-fill icon-stroke-hover icon-fill-hover">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+          />
+        </svg>
+
         </div>
         
         <div className="col-start-4 col-end-5 row-start-2 row-end-2">

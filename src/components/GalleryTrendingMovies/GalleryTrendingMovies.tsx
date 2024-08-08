95b87a040c84e19ada2f7fa7d99e0cb4a0627df8
Christian Scholtysik <christianscholtysik@gmail.com>
2024-08-08 08:52:22 +0200

    rounded corner in gallery

--- src/components/GalleryTrendingMovies/GalleryTrendingMovies.tsx
+++ src/components/GalleryTrendingMovies/GalleryTrendingMovies.tsx
@@ -9,16 +9,16 @@ const GalleryTrendingMovies: React.FC<GalleryTrendingMoviesProps> = ({
   movies,
 }) => {
   return (
-    <div className="flex overflow-x-auto space-x-4 py-4">
+    <div className="flex overflow-x-auto space-x-4 py-4 ">
       {movies.slice(0, 4).map((movie) => (
         <div
           key={movie.id}
-          className="flex-shrink-0 w-full h-64 relative bg-cover bg-top rounded-lg"
+          className="flex-shrink-0 w-full h-64 relative bg-cover bg-top rounded-3xl"
           style={{
             backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`,
           }}>
           <Link to={`/detail/${movie.id}`} className="absolute inset-0 ">
-            <div className="border-2 flex flex-col justify-end items-center h-full bg-gradient-to-t from-black via-transparent to-transparent rounded-lg pb-4">
+            <div className="border-2 flex flex-col justify-end items-center h-full bg-gradient-to-t from-black via-transparent to-transparent rounded-3xl pb-4 ">
               <h2 className="  text-white text-lg truncate ">{movie.title}</h2>
               <div className="gap-2 flex">
                 <img src={"/Star.svg"} alt="Star" />
